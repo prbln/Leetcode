f6720463bbb965bd899146cceef04426d7060a53
Prabhleen Kaur Saini <54107438+prbln@users.noreply.github.com>
2023-06-19 12:28:57 -0500

    Time: 83 ms (11.10%) | Memory: 16.5 MB (52.76%) - LeetSync

--- min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -1,11 +1,21 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        dp = [0]*(len(cost)+1)
-        def recursive_sol(ind):
-            if ind<2:
-                return 0
-            dp[ind-1] = dp[ind-1] if dp[ind-1] else recursive_sol(ind-1) + cost[ind-1]
-            dp[ind-2] = dp[ind-2] if dp[ind-2] else recursive_sol(ind-2) + cost[ind-2] 
-            dp[ind] = min(dp[ind-1] ,dp[ind-2])
-            return dp[ind]
-        return recursive_sol(len(cost))
+        n = len(cost) 
+        dp = [0]*(n+1)
+        # def recursive_sol(ind):
+        #     if ind<2:
+        #         return 0
+        #     dp[ind-1] = dp[ind-1] if dp[ind-1] else recursive_sol(ind-1) + cost[ind-1]
+        #     dp[ind-2] = dp[ind-2] if dp[ind-2] else recursive_sol(ind-2) + cost[ind-2] 
+        #     dp[ind] = min(dp[ind-1] ,dp[ind-2])
+        #     return dp[ind]
+        # return recursive_sol(len(cost))
+        for i in range(n+1):
+            if i>=2:
+                dp[i] = min(dp[i-1]+ cost[i-1] ,dp[i-2] + cost[i-2]) 
+        return dp[n]
+
+                
+
+
+ 
