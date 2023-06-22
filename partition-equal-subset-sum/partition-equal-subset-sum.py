bb745f33a08aaf7d1d07e880884abb582fdbd7b5
Prabhleen Kaur Saini <54107438+prbln@users.noreply.github.com>
2023-06-22 10:27:38 -0500

    Time: 3734 ms (12.26%) | Memory: 32 MB (44.66%) - LeetSync

--- partition-equal-subset-sum/partition-equal-subset-sum.py
+++ partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -1,34 +1,27 @@
 from collections import defaultdict
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        summ = sum(nums)
-        if summ%2 != 0:
+        ind = len(nums)
+        totalSum = sum(nums)//2
+        if sum(nums)%2 !=0:
             return False
-        target = summ//2
-        dp = [[False]*(target+1) for _ in range(len(nums))]
-        # def targetcheck(ind,target):
-        #     if ind>=len(nums):
-        #         return False
-        #     if target ==0:
-        #         return True
-        #     ele = nums[ind]
-        #     dp[ind+1][target]  = dp[ind+1][target] if dp[ind+1][target]  else targetcheck(ind+1, target )
-             
-        #     dp[ind+1][target-ele]  =  dp[ind+1][target-ele]  if dp[ind+1][target-ele] else targetcheck(ind+1, target-ele )
-        #     return dp[ind+1][target] or dp[ind+1][target-ele]
+        dp = [[0]*(totalSum+1)for _ in range(ind)]
+        # if nums[0]
+        if nums[0]<=totalSum:
+            dp[0][nums[0]] = True 
+        else:
+            return False
+        for i in range(1,ind):
+            for j in range(totalSum+1):
+                noTake, take = False, False
+                if j-nums[i]>=0:
+                    val = j-nums[i]
+                    take = dp[i-1][val]
+                noTake = dp[i-1][j] 
+                dp[i][j] = take or  noTake  
+            # print(dp,i,j)
+        # print(dp) 
+        return dp[ind-1][totalSum]           
 
-        # return targetcheck(0,target)
-        for i in range(len(nums)):
-            dp[i][0] = True
-        if nums[0] <= target:
-            dp[0][nums[0]] = True
 
-        for i in range(1,len(nums)):
-            for j in range(1,target+1):
-                if j>=nums[i]:
-                    dp[i][j] = dp[i-1][j] or dp[i-1][j-nums[i]]
-                else:
-                    dp[i][j] = dp[i-1][j]
-        # print(len(nums)-1,target)
-        # print(dp)
-        return dp[len(nums)-1][target]
+
