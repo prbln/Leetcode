6025c3937adc331e0eae3d4ef82774c624be4859
Prabhleen Kaur Saini <54107438+prbln@users.noreply.github.com>
2023-06-23 11:53:42 -0500

    Time: 917 ms (5.03%) | Memory: 16.8 MB (69.78%) - LeetSync

--- target-sum/target-sum.py
+++ target-sum/target-sum.py
@@ -1,21 +1,29 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         n = len(nums)
-        dictt = {}
-
-        def checkTarget(ind, summ, count):
-            # print(summ)
-            if target == summ and ind == n:
-                count+=1
-                return count
-            if ind>=n :
-                return count 
-            if (ind,summ) in dictt:
-                return dictt[(ind,summ) ]
+        if n ==1 :
+            if target == nums[0] or target == -1*nums[0]:
+                return 1 
             else:
-                take_positive = checkTarget(ind+1, summ  + nums[ind], count) 
-                take_negative = checkTarget(ind+1, summ - nums[ind], count)
-                dictt[(ind,summ)] = take_positive + take_negative
-            # print(take_positive,take_negative )
-            return dictt[(ind,summ)]
-        return checkTarget(0,0, 0)
+                return 0      
+        ts = sum(nums)
+        dp = [[0]*(2*ts + 1) for _ in range(n)]
+        if ts < target: 
+            return 0
+        if -1*ts>target:
+            return 0
+        mid = ts
+        
+        dp[0][mid+nums[0]], dp[0][mid-nums[0]] = 1,1
+        if mid+nums[0] ==mid-nums[0]:
+            dp[0][mid+nums[0]]+=1
+        for i in range(1,n):
+            for j in range(-1*ts,ts+1):
+                takeP,takeN = 0,0
+                if j+nums[i]<=ts:
+                    takeP = dp[i-1][mid+j+nums[i]]
+                if j-nums[i] >=-1*ts:
+                    takeN = dp[i-1][mid+(j-nums[i])]
+                dp[i][mid+j] = takeP+takeN
+        # print(mid,ts)
+        return dp[n-1][mid+target]
