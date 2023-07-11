b74def708f680c219da20ff19f78451a874d001f
Prabhleen Kaur Saini <54107438+prbln@users.noreply.github.com>
2023-07-11 09:01:43 -0500

    Time: 176 ms (36.16%) | Memory: 20.1 MB (23.57%) - LeetSync

--- edit-distance/edit-distance.py
+++ edit-distance/edit-distance.py
@@ -4,12 +4,14 @@ class Solution:
         m = len(word2)
         dp = [[0]*(m+1)for _ in range(n+1)]
         dp[0][0] = 0 
-        for j in range(1,m+1):
-            dp[0][j] = j
-        for i in range(1,n+1):
-            dp[i][0] = i
-        for i in range(1,n+1):
-            for j in range(1,m+1):
+        for i in range(0,n+1):
+            for j in range(0,m+1):
+                if i ==0:
+                    dp[0][j] = j
+                    continue
+                if j ==0:
+                    dp[i][0] = i
+                    continue
                 if word1[i-1] == word2[j-1]:
                     dp[i][j] =  dp[i-1][j-1]
                 else:
