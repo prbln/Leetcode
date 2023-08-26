52bac0d7efe34b9ecb92cbc5c5b3a7b613530611
Prabhleen Kaur Saini <54107438+prbln@users.noreply.github.com>
2023-08-26 14:02:15 -0500

    Time: 34 ms (90.29%) | Memory: 16.3 MB (47.23%) - LeetSync

---
+++ 54-spiral-matrix/spiral-matrix.py
@@ -0,0 +1,33 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        minRow = 0 
+        maxRow = len(matrix)-1
+        minCol = 0 
+        maxCol = len(matrix[0])-1
+        ans = []
+        while(minRow<=maxRow and minCol<=maxCol):
+            col = minCol 
+            
+            while(col<=maxCol and minRow<=maxRow and minCol<=maxCol):
+                ans.append(matrix[minRow][col])
+                col+=1
+            minRow+=1
+            row= minRow
+            while(row<=maxRow and minRow<=maxRow and minCol<=maxCol):
+                ans.append(matrix[row][maxCol])
+                row+=1
+            maxCol-=1
+            col = maxCol
+            # print(col,minCol)
+            while(col>=minCol and minRow<=maxRow and minCol<=maxCol):
+                ans.append(matrix[maxRow][col])
+                col-=1
+            maxRow -=1
+            row = maxRow
+            while(row>=minRow and minRow<=maxRow and minCol<=maxCol):
+                ans.append(matrix[row][minCol])
+                row-=1
+            minCol+=1 
+        return ans
+
+
