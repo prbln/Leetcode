ff17c1565b2a6c6d3d34ef83702d3a33552ebebb
Prabhleen Kaur Saini <54107438+prbln@users.noreply.github.com>
2023-11-06 14:34:28 -0600

    Time: 59 ms (19.80%) | Memory: 16.1 MB (83.43%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,24 +1,31 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        roman_numerals = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000
+        romanNumerals = {
+            "I" :1, 
+            "IV": 4,
+            "V" : 5,
+            "IX" : 9, 
+            "X" : 10, 
+            "L" : 50,
+            "XL" : 40,
+            "C": 100, 
+            "XC": 90, 
+            "D": 500, 
+            "CD": 400,
+            "M" : 1000,
+            "CM" : 900
         }
-        integerFrom = []
-        ans = 0 
-        for number in s:
-            integerFrom.append(roman_numerals[number])
-        for i in range(len(integerFrom)-1):
-            if integerFrom[i+1]>integerFrom[i]:
-                ans+= -1*integerFrom[i]
-            else:
-                ans+=integerFrom[i]
-        return ans+integerFrom[-1]
-
+        val = 0 
+        i = 0 
+        n = len(s)
+        while(i<n):
+            if i != n-1:
+                if romanNumerals.get(s[i]+s[i+1],0):
+                    val += romanNumerals[s[i]+s[i+1]]
+                    i+=2
+                    continue
+            val += romanNumerals[s[i]]
+            i+=1
+        return val
 
             
