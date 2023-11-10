06eeb017e78dceab1041b77c50ae9737030f3930
Prabhleen Kaur Saini <54107438+prbln@users.noreply.github.com>
2023-11-10 12:26:09 -0600

    Time: 52 ms (80.37%) | Memory: 16.3 MB (63.69%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,48 +1,34 @@
 from collections import deque
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
+        n,m = len(grid), len(grid[0])
         q = deque()
-        n = len(grid)
-        m = len(grid[0])
-        visited = dict()
-        noFresh = True
+        visited = [[0 for j in range(m)] for i in range(n)]
+        numOfFresh = 0 
+        time = 0 
         for i in range(n):
             for j in range(m):
-                if grid[i][j] ==2:
-                    q.append([i,j])
-                if grid[i][j] ==1:
-                    noFresh = False
-        def BFS(queue, visted):
-            minute=0
-            while(len(queue)!=0):
-                minute+=1
-                print(queue)
-                for i in range(len(queue)):
-                    r,c = queue.popleft()
-                    visited[(r,c)] = True
-                    # if grid[r][c] == 2:
+                if grid[i][j] == 2:
+                    q.append((i,j,0))
+                elif grid[i][j] == 1:
+                    numOfFresh+=1
+        while(len(q)!= 0):
+            r,c,t = q.popleft()
+            visited[r][c] = True
+            time =max(t, time)
+            if grid[r][c] == 1:
+                numOfFresh-=1
+            nextEle =[[-1,0],[1,0],[0,-1],[0,1]]
+            for i,j in nextEle:
+                nextRow = i + r
+                nextCol = j + c
+                if min(nextRow,nextCol )>=0 and nextRow<n and nextCol<m and grid[nextRow][nextCol] == 1 and not visited[nextRow][nextCol] :
+                    q.append((nextRow,nextCol,t+1))
+                    visited[nextRow][nextCol] = True
+        if numOfFresh >0:
+            return - 1
+        return time
 
-                    adjRow = [-1,0,1,0]
-                    adjCol = [0,-1,0,1]
-                    for i in range(4):
-                        nxtRow = r + adjRow[i]
-                        nxtCol = c + adjCol[i]
-                        if nxtRow<n and nxtRow>=0 and nxtCol<m and nxtCol>=0 and not visited.get((nxtRow,nxtCol), False) and grid[nxtRow][nxtCol] !=0:
-                            # print(nxtRow,nxtCol )
-                            grid[nxtRow][nxtCol] =2
-                            queue.append([nxtRow, nxtCol])
-                            visited[(nxtRow,nxtCol)] = True
-            return minute              
-        if noFresh:
-            return 0
-        ans = BFS(q,visited) -1
-        # print(ans)
-        for i in range(n):
-            for j in range(m):
-                if grid[i][j] ==1:
-                    return -1 
-        return ans
 
-        
-        
-        
+                
+            
