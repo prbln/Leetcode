c9f63d0f825c619c1e1b397250ed2135e1297c89
Prabhleen Kaur Saini <54107438+prbln@users.noreply.github.com>
2023-11-11 12:27:48 -0600

    Time: 334 ms (26.72%) | Memory: 26.2 MB (17.80%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,38 +1,29 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-
-        def dfs(s):
-            # print("yo")
-            
-            while(len(s)>0):
-                # print(visited, s)
-                r,c = s.pop()
-                visited[r][c] = True 
-                adjRows = [-1,0,1,0]
-                adjcol = [0,1,0,-1]
-
-                for i in range(4):
-                    nextrow = r + adjRows[i]
-                    nextcol = c + adjcol[i]
-                    if nextrow >= 0 and nextrow<n and nextcol>=0 and nextcol<m and grid[nextrow][nextcol] =="1" and visited[nextrow][nextcol] ==False  :
-                        s.append((nextrow,nextcol))
-            
-
         n = len(grid)
         m = len(grid[0])
-        visited = [[ False for j in range(m)] for i in range(n)]
-        # print(n,m, len(visited), len(visited[0]))
+        visited = dict()
+        stack = []
+        def dfs(stack):
+            while(len(stack) != 0 ):
+                i, j = stack.pop()
+                visited[(i,j)] = True
+                nextEle = [[-1,0],[1,0],[0,-1],[0,1]]
+                for r,c in nextEle:
+                    nextR = i + r
+                    nextC = j + c
+                    if nextR < n and nextC < m and min(nextR, nextC)>=0 and not visited.get((nextR, nextC), False) and grid[nextR][nextC] ==   "1":
+                        stack.append((nextR, nextC)) 
+                        visited[(nextR, nextC)] = True
         count = 0 
         for i in range(n):
             for j in range(m):
-                # print
-                if visited[i][j] == False and grid[i][j] =="1":
-                    # print(i,j)
-                    stack = [(i,j)]
-                    dfs(stack)
+                if grid[i][j] =="1" and not visited.get((i,j), False):
+                    stack.append((i,j))
+                    visited[(i,j)] = True
                     count+=1
-                visited[i][j] = True
+                    dfs(stack)
+                    
+        return count
         
-        return count  
-                
-                
+        
