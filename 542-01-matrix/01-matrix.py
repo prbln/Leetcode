fc934f0e62f74121488516dcdd368a8b1ff04202
Prabhleen Kaur Saini <54107438+prbln@users.noreply.github.com>
2023-11-16 16:35:09 -0500

    Time: 691 ms (15.14%) | Memory: 20.4 MB (24.12%) - LeetSync

---
+++ 542-01-matrix/01-matrix.py
@@ -0,0 +1,30 @@
+from collections import deque
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        n = len(mat)
+        m = len(mat[0])
+        q = deque()
+        visited = dict()
+        def bfs(q):
+            while(len(q)!= 0):
+                i,j, level = q.popleft()
+                visited[(i,j)] = True
+                nextEle = [[-1,0],[1,0],[0,-1],[0,1]]
+                mat[i][j] = level
+                for r,c in nextEle:
+                    nextr = r + i 
+                    nextc = c + j 
+                    if min(nextr, nextc) >=0 and nextr<n and nextc<m and not visited.get((nextr,nextc), False ) and mat[nextr][nextc] != 0 :
+                        q.append((nextr, nextc,level+1) )
+                        mat[nextr][nextc] = level +1
+                        visited[(nextr,nextc)] = True
+        for i in range(n):
+            for j in range(m):
+                if mat[i][j] ==0:
+                    q.append((i,j,0))
+        bfs(q)
+        return mat
+
+                
+
+            
