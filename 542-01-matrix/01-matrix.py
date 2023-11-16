0aa1864cc65333bda1b220b6657cd88b99ab7a7f
Prabhleen Kaur Saini <54107438+prbln@users.noreply.github.com>
2023-11-16 16:47:58 -0500

    Time: 601 ms (35.68%) | Memory: 19 MB (74.22%) - LeetSync

--- 542-01-matrix/01-matrix.py
+++ 542-01-matrix/01-matrix.py
@@ -4,24 +4,23 @@ class Solution:
         n = len(mat)
         m = len(mat[0])
         q = deque()
-        visited = dict()
 
         for i in range(n):
             for j in range(m):
                 if mat[i][j] ==0:
                     q.append((i,j,0))
+                else:
+                    mat[i][j] = -1
         while(len(q)!= 0):
             i,j, level = q.popleft()
-            visited[(i,j)] = True
             nextEle = [[-1,0],[1,0],[0,-1],[0,1]]
             mat[i][j] = level
             for r,c in nextEle:
                 nextr = r + i 
                 nextc = c + j 
-                if min(nextr, nextc) >=0 and nextr<n and nextc<m and not visited.get((nextr,nextc), False ) and mat[nextr][nextc] != 0 :
+                if min(nextr, nextc) >=0 and nextr<n and nextc<m and mat[nextr][nextc] == -1 and mat[nextr][nextc] != 0 :
                     q.append((nextr, nextc,level+1) )
                     mat[nextr][nextc] = level +1
-                    visited[(nextr,nextc)] = True
         return mat
 
                 
