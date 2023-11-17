c85106b494f42cd2bad9c1b4536427f37159c860
Prabhleen Kaur Saini <54107438+prbln@users.noreply.github.com>
2023-11-17 15:43:39 -0500

    Time: 145 ms (19.25%) | Memory: 16.9 MB (74.33%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -1,33 +1,29 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-
-        def dfs(s):
-            area = 0 
-            adjrows = [-1,0,1,0]
-            adjcols = [0,1,0,-1]
-            while(len(s)>0):
-                # print(s)
-                r,c = s.pop()
-                visited[r][c] = True
-                area+=1
-                for i in range(4):
-                    nxtrow = r + adjrows[i]
-                    nxtcol = c + adjcols[i]
-                    if nxtrow>=0 and nxtrow<n and nxtcol>=0 and nxtcol<m and visited[nxtrow][nxtcol] == False and grid[nxtrow][nxtcol] ==1:
-                        visited[nxtrow][nxtcol] = True
-                        s.append((nxtrow,nxtcol))
-            return area
-        #step 01 visted matrix
+        maxArea = 0
         n = len(grid)
         m = len(grid[0])
-        visited = [[False for j in range(m)] for i in range(n)]
-        maxi =0 
-        #traversal do dfs whenever you see a one 
-
+        visited = dict()
+        def dfs(stack):
+            
+            count = 0 
+            while(len(stack)!= 0 ):
+                i, j = stack.pop()
+                visited[(i, j)] = True
+                count+=1 
+                nextEle = [[0,1],[0,-1],[1,0],[-1,0]]
+                for r,c in nextEle:
+                    nextr = i + r
+                    nextc = j + c
+                    if min(nextr, nextc) >=0 and nextr<n and nextc<m and grid[nextr][nextc] == 1 and not visited.get((nextr, nextc), False):
+                        stack.append((nextr, nextc))
+                        visited[(nextr, nextc)] = True
+            return count
         for i in range(n):
             for j in range(m):
-                if visited[i][j] ==False and grid[i][j] ==1:
-                    s = [(i,j)]
-                    maxi = max(maxi, dfs(s))
-                visited[i][j] ==True 
-        return maxi
+                if grid[i][j] ==1 and not visited.get((i,j), False):
+                    area = dfs([(i,j)])
+                    maxArea = max(maxArea, area )
+
+        return maxArea
+         
