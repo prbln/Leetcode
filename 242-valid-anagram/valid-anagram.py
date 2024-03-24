a397b9a2e75c6b3a069cbc1e3881a387849e22a5
Prabhleen Kaur Saini <54107438+prbln@users.noreply.github.com>
2024-03-24 16:13:05 -0500

    Time: 50 ms (54.35%) | Memory: 17 MB (65.10%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,15 +1,25 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        count = dict()
-        for letter in s:
-            if not count.get(letter):
-                count[letter] = 0 
-            count[letter] +=1  
-        for letter in t:
-            if count.get(letter) == None or count[letter] == 0 :
-                return False 
+        letter_frequency_s = dict() 
+        letter_frequency_t = dict()
+        #Step -1: iterate over string s save count freq of each letter s O(26), O(n)
+        for letter in s: 
+            if  letter_frequency_s.get(letter, False):
+                letter_frequency_s[letter]+=1
             else:
-                count[letter]-=1
-        if len(s) == len(t) :
-            return True
-        return False
+                letter_frequency_s[letter]=1
+        #Step -2: iterate over string t save count freq of each letter s O(26), O(n) 
+        for letter in t: 
+            if  letter_frequency_t.get(letter, False):
+                letter_frequency_t[letter]+=1
+            else:
+                letter_frequency_t[letter]=1
+        #match the freq of each letter in both the cases 
+        for ascii_value in range(ord('a'), ord('z') + 1):
+            alphabet = chr(ascii_value)#O(26)
+            if letter_frequency_s.get(alphabet, 0 ) != letter_frequency_t.get(alphabet, 0):
+                return False 
+        return True
+
+# letter_frequency_s = { a:3, n:1, g:1, r:1, m:1 }
+# letter_frequency_t = { a:3, n:1, g:1, r:1, m:1 }
